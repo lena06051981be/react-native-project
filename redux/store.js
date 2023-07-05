8bd41f5d928fcd8512b631f80751dcc0015ae71a
Elena Solomko <lena06051981be@gmail.com>
2023-07-05 14:08:52 +0200

    update

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { authSlice } from "./auth/authSlice";
 
-const rootReduce = combineReducers({
-    [authSlice.name]: authSlice.reducer,
-});
+// const rootReduce = combineReducers({
+//     [authSlice.name]: authSlice.reducer,
+// });
 
 export const store = configureStore({
-    reducer: rootReduce,
+    reducer: {
+        [authSlice.name]: authSlice.reducer,
+    }
 });
